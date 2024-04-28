1dc17afb5002fc6652d5bd836ba5b9f909469de7
TharinduMahesh <155628660+TharinduMahesh@users.noreply.github.com>
2024-04-28 21:16:16 +0530

    Update Hello World

--- app.vue
+++ app.vue
@@ -3,6 +3,8 @@
     <h1>Tharindu Mahesh</h1>
     <p >DEV LABS</p>
     <h1 style="font-size: 100px;">diva diva diva </h1>
+    <h3>HEllo World</h3>
+    <h4>Hello World 2</h4>
 
   </div>
 </template>
