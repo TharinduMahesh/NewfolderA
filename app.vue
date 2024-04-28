e21ffcef25dd93dfbec97b2a1c9779ca4670ea63
TharinduMahesh <155628660+TharinduMahesh@users.noreply.github.com>
2024-04-28 19:50:22 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Tharindu Mahesh</h1>
+    <p >DEV LABS</p>
+    <h1 style="font-size: 100px;">diva diva diva </h1>
+
+  </div>
+</template>
+
+
+<script setup>
+</script>
+
+
+
+<style>
+</style>
